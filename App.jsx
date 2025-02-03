47240e10cb636aac1514e38d27b15c37251807b1
Deepro Chakraborty <deepro_19@rediffmail.com>
2025-02-03 14:17:40 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,15 @@
+
+
+import React from "react";
+import TodoApp from "./Components/TodoApp";
+
+function App() {
+  return (
+    <div>
+      <h1>Optimized Todo App</h1>
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
